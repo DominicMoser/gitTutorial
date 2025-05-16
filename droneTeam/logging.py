258eebfc9dd586c047d5b5b948b7a8a0f83bab6a
Dejan <Dejan.Antolovic@loewenrot.de>
2025-05-16 15:53:13 +0200

    Logging Funktion mit Ausgabezeit erstellt

--- droneTeam/logging.py
+++ droneTeam/logging.py
@@ -1,2 +1,8 @@
+from datetime import datetime
+
 def log(message:str):
-    pass
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print( ( ( "|" + current_time ) + ("| " + message) ) )
+
+log("es geht")
