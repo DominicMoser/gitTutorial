0d4232fe65f2cae8c18ac9d2adae1e8632820fb7
Dejan <Dejan.Antolovic@loewenrot.de>
2025-05-30 15:25:28 +0200

    Script gibt die Nachrichten in die Datei test.log aus

--- droneTeam/logging.py
+++ droneTeam/logging.py
@@ -4,6 +4,8 @@ def log(message:str):
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print( ( ( "|" + current_time ) + ("| " + message) ) )
+    with open("test.log", "a") as f:
+        f.write(( ( "|" + current_time ) + ("| " + message) + "\n" ))
 
 if __name__ == "__main__":
     log("Script Running")
